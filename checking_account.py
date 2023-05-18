3e0667266a43f3c6519b8000354339aa66f75c3a
bwilkins96 <ben.jr.speed@gmail.com>
2023-05-18 10:30:17 -0400

    checking account class

---
+++ checking_account.py
@@ -0,0 +1,33 @@
+# SWDV 630 - Object-Oriented Software Architecture
+# Checking account class
+
+class CheckingAccount:
+    def __init__(self, name, address, acc_num):
+        self._name = name.capitalize()
+        self._address = address
+        self._account_number = acc_num
+        self._balance = 0
+
+    def name(self):
+        return self._name
+    
+    def address(self):
+        return self._address
+    
+    def account_number(self):
+        return self._account_number
+    
+    def balance(self):
+        return self._balance
+    
+    def set_name(self, name):
+        self._name = name.capitalize()
+
+    def set_address(self, address):
+        self._address = address
+
+    def credit(self, amt):
+        self._balance += amt
+
+    def debit(self, amt):
+        self._balance -= amt
